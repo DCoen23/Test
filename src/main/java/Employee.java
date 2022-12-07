0e4b180c368a7fd651378b7ffce037a7d3f9c322
DARRAGH COEN - STUDENT <G00370083@atu.ie>
2022-12-07 09:44:58 +0000

    added set method and employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String name;
+    private long PPSid;
+    private String type;
+    private String gender;
+    private int age;
+    
+    public Employee(String name, long PPSid, String type, String gender, int age){
+        setName(name);
+        setPPSid(PPSid);
+        setType(type);
+        setGender(gender);
+        setAge(age);
+    }
+
+    private void setName(String name) {
+    }
+
+    private void setPPSid(long ppSid) {
+    }
+
+    private void setType(String type) {
+    }
+
+    private void setGender(String gender) {
+    }
+
+    private void setAge(int age) {
+    }
+
 }
